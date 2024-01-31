8c38c53e0feeb36920e6c13776c8502d46a7a6c4
yandexads <91550089+yandexads@users.noreply.github.com>
2024-01-31 22:45:21 +0300

    Add files via upload

---
+++ mostriki/scripts/project/scriptsInEvents.js
@@ -0,0 +1,18 @@
+
+
+
+const scriptsInEvents = {
+
+	async Menuevent_Event7_Act4(runtime, localVars)
+	{
+		localVars.variantName = new Date().toLocaleString('en', {
+		 year: 'numeric',
+		 month: '2-digit',
+		 day: '2-digit',
+		});
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
