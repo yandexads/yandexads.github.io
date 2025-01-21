88fc3c940920d07099fb831f1134bfaaeadb0b0a
yandexads <91550089+yandexads@users.noreply.github.com>
2025-01-21 22:02:01 +0300

    Add files via upload

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -1,40 +1,4 @@
-// Инициализация необходимых скриптов
-const loadScripts = () => {
 
-    const scriptTelegram = document.createElement('script');
-    scriptTelegram.src = "https://telegram.org/js/telegram-web-app.js";
-    document.head.appendChild(scriptTelegram);
-	
-	const scriptTads = document.createElement('script');
-    scriptTads.src = "https://w.tads.me/widget.js";
-    document.head.appendChild(scriptTads);
-};
-
-// Функция инициализации Tads виджета
-const initTadsWidget = (id, debug, onShowReward, onClickReward, onAdsNotFound) => {
-    const adController = window.tads.init({ widgetId: id, debug: false, onShowReward, onClickReward, onAdsNotFound });
-    adController.loadAd()
-        .then(() => adController.showAd())
-        .catch((result) => {
-            console.log(result);
-        });
-};
-
-// Callbacks для рекламы
-const onShowRewardCallback = (result) => {
-    console.log('Показан рекламный контент, вознаградите пользователя:', result);
-};
-
-const onClickRewardCallback = (result) => {
-    console.log('Клик на рекламу, вознаградите пользователя:', result);
-};
-
-const onAdsNotFound = () => {
-    console.log('Не удалось найти рекламу для показа');
-};
-
-// Загрузка необходимых скриптов
-loadScripts();
 
 
 const scriptsInEvents = {
