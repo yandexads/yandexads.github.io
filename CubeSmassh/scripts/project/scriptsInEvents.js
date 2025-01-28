185f696e51b3e8208afda39adbc3d724d986a329
yandexads <91550089+yandexads@users.noreply.github.com>
2025-01-29 00:05:11 +0300

    Add files via upload

---
+++ CubeSmassh/scripts/project/scriptsInEvents.js
@@ -0,0 +1,33 @@
+
+
+
+const scriptsInEvents = {
+
+	async Game_Event37_Act1(runtime, localVars)
+	{
+		// Попробуем инициировать и показать баннер
+		try {
+		    // Инициализация Adsgram и показ баннера
+		    await window.Adsgram.init({ blockId: "int-7493" }).show();
+		} catch (error) {
+		    // Обработка ошибок: ничего не делаем или выполняем другие действия
+		    console.error("Ошибка при показе рекламы:", error);
+		}
+	},
+
+	async Game_Event85_Act6(runtime, localVars)
+	{
+		// Попробуем инициировать и показать баннер
+		try {
+		    // Инициализация Adsgram и показ баннера
+		    await window.Adsgram.init({ blockId: "int-7493" }).show();
+		} catch (error) {
+		    // Обработка ошибок: ничего не делаем или выполняем другие действия
+		    console.error("Ошибка при показе рекламы:", error);
+		}
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
